a9cd31039b01d68fae6243eaea6d864b0d300af3
Durga Saikumar Reddy Dwarampudi <durgasaikumarreddy@gmail.com>
2021-09-29 18:42:11 +0530

    production changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,8 +90,9 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = {:host => 'serene-headland-62639.herokuapp.com'}
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
