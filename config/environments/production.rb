8f4ab28d7666b2c4e51a89c6a08012f4149df870
Durga Saikumar Reddy Dwarampudi <durgasaikumarreddy@gmail.com>
2021-09-29 19:23:22 +0530

    reverting changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -76,7 +76,6 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
-
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
@@ -91,15 +90,5 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
-  
-  config.action_mailer.default_url_options = {:host => 'serene-headland-62639.herokuapp.com'}
   config.active_record.dump_schema_after_migration = false
-
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    user_name:            'sai2kumar5@gmail.com',
-    password:             '14354-Ee-003',
-    authentication:       'plain',
-    enable_starttls_auto: true }
 end
