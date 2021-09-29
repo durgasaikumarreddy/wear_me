4d0bea436554e7e274dff484527ec012e4565215
Durga Saikumar Reddy Dwarampudi <durgasaikumarreddy@gmail.com>
2021-09-29 18:59:03 +0530

    production changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,7 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
@@ -90,15 +91,13 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
-  config.action_mailer.raise_delivery_errors = true
+  
   config.action_mailer.default_url_options = {:host => 'serene-headland-62639.herokuapp.com'}
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :smtp
+
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'example.com',
     user_name:            'sai2kumar5@gmail.com',
     password:             '14354-Ee-003',
     authentication:       'plain',
